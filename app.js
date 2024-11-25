ef00bef6d0cac423792fbf5160b89421f5bb736c
kgan3008 <kalash.gangwal@7-11.com>
2024-11-25 20:51:15 +0530

    understand jsx

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client'
-// const heading = React.createElement('h1',{id:"heading"},"Hello World from React")
-// const parent = React.createElement(
-//     'div',
-//     { id: 'parent' },
-//     React.createElement('div', { id: 'child' }, React.createElement('h1', {}, 'this is h2 tags'))
-// );
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// console.log(parent);
-// root.render(parent);
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+//creating React element using react 
+const heading  = React.createElement('h1',{id:'heading'},'Kalash React code');
+console.log(heading);//this is react element
+
+//react element is nothing but an javascript object not an html element 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//root element where the whole react elemnt is rendered it is the parent most element
+
+//createElement -> react element(js object) => (rendering on to the dom it converts to html and then it push to the dom)
 
 
-//  <div id ="parent">
-//     <div id = "child1">
-//         <h1>i am h1 tag of child1</h1>
-//         <h2>i ma h2 tag of child1</h2>
-//     </div>
-//     <div id = "child2">
-//         <h1>i am h1 tag of child2</h1>
-//         <h2>i ma h2 tag of child2</h2>
-//     </div>
-// </div> 
 
-// const parent = React.createElement("div", { id: 'parent' }, [React.createElement('div', { id: 'child1' }, [
-//     React.createElement('h1', {}, 'i am h1 tag of child1'),
-//     React.createElement('h2', {}, 'i am h2 tag of child1')
-// ]), React.createElement('div', { id: 'child2' }, [
-//     React.createElement('h1', {}, 'i am h1 tag of child2'),
-//     React.createElement('h2', {}, 'i am h2 tag of child2')
-// ])])
 
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(parent);
-document.getElementById("loadButton").addEventListener("click", () => {
-    import("./lazyModule.js").then((module) => {
-      module.sayHello();
-    });
-  });
-  
+//creating react element using jsx 
+
+const jsxHeading = <h1 id='heading'>Kalash React code</h1>
+const multiJsx = (
+                 <h1 className="head">
+                    hello
+                    </h1>
+                   )  //multiline jsx
+//when this line executes jsx becomes react element
+
+const HeadingComponent = ()=>{
+   return <h1 className='heading'>Namste javascript functional component</h1>
+}
+const HeadingComponent1 = ()=> <h1 className='heading'>Namste javascript functional component1</h1>
+
+const title = (
+   <h1>React element title</h1>
+)
+const elem = (
+   <div id='container'>
+   <h1 className='heading'>Namste javascript functional component2</h1>
+   {title}
+   </div>
+)
+const HeadingComponent2 =()=>(
+   <div id='container'>
+   <h1 className='heading'>Namste javascript functional component2</h1>
+   {title}
+   </div>
+)
+
+const TitleComponent =()=>(
+ <h1 className='"head' tabIndex="5">Namaste Ttile</h1>
+)
+console.log(jsxHeading)
+root.render(<HeadingComponent2 />) //this is react element
